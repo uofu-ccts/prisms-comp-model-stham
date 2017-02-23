32171fc0040b452992b0d8824441ba09d5f3fe13
u0403692 <a.m.lund@utah.edu>
2017-02-23 15:43:04 -0700

    refactored demoActPlot

--- test/actprofile.py
+++ test/actprofile.py
@@ -16,10 +16,120 @@ import os;
 
 np.set_printoptions(threshold=np.inf)
 
+def demoActPlot(frame,labelcolumn, prefix):
+	#prelim stuff
+	mapping = np.sort(list(set(jointable['TRCODE'])))
+	actcount = len(mapping)
+	tri = { tr:i for i,tr in enumerate(mapping) }
+	
+	#set up coloration
+	start = 0.0
+	stop = 1.0
+	number_of_lines=19
+	cm_subsection = np.linspace(start, stop, number_of_lines)
+	maincolors = [ cm.prism(x) for x in cm_subsection ]
+	colors = []
+	for i in mapping:
+		tier1 = i // 10000;
+		tier2 = (i // 100) - (tier1 * 100)
+		if (tier1 == 50): tier1 = 19;
+		if (tier2 == 99): tier2 = 19; 
+		tier1 = tier1 - 1;
+		tier2 = tier2 - 1
+		
+		scol = list(maincolors[tier1])
+	# 	scol[2] = scol[2] + (tier2 * 0.1)
+	# 	if(scol[2] > 1.0): scol[2] = 1.0;
+	# 	scol[3] = scol[3] + (tier2 * 0.1)
+	# 	if(scol[3] > 1.0): scol[3] = 1.0;
+		colors += [tuple(scol)];
 
-# labels = dblabels;
-# labels,labelsetcount = labelReduce(labels);
-# labelscount = Counter(labels);
+
+	#set up labeling
+	legart = []
+	leglabels=["Personal Care","HH activities","HH Member Care","NonHH care","work","Edu", "Consumerism", "Pro Services", "HH services", "Gov services", "Eating/Drinking","SocialRelaxLeisure","Sports/Excercise","Religious","Volunteering", "Telephone","Null","Traveling", "Other"]
+	for i in range(number_of_lines):
+		legart += [mpatch.Rectangle((0,0),1,1,fc=maincolors[i])]
+
+	framegroup = frame.groupby(labelcolumn);
+	labelscount = Counter(frame[frame["TUACTIVITY_N"] == 1][labelcolumn]);
+	#jg is for joingroup
+	for jg in framegroup:
+		
+		if(labelscount[jg[0]] < 25):
+			continue;
+		
+		daycount = np.zeros(7)
+		data = np.zeros([actcount,288*7])
+		sum = np.zeros(288*7)
+		dayset = {1}
+		
+		#print(jg[1])
+		subsetjg = jg[1][jg[1]['TUACTIVITY_N'] == 1]
+		mdem = subsetjg.groupby(["TESEX","TEAGE"]).TESEX.count()[1]
+		fdem = subsetjg.groupby(["TESEX","TEAGE"]).TESEX.count()[2]
+		#print(mdem,fdem);
+		if(type(mdem) == pd.core.series.Series): mdem.reindex(np.arange(0,101)).fillna(int(0)).plot(color='b')
+		else: print("bad mdem:",mdem,type(mdem))
+		if(type(fdem) == pd.core.series.Series): fdem.reindex(np.arange(0,101)).fillna(int(0)).plot(color='r')
+		else: print("bad fdem:",fdem,type(fdem))
+
+		plt.title( "Label " +str(jg[0])+", count: "+str(labelscount[jg[0]]) )
+		F = plt.gcf();
+		F.set_size_inches(4,3)
+		F.set_dpi(300);
+		F.savefig(prefix + "demo-" +str(jg[0]) + ".png");
+		plt.clf();
+		
+		
+		for ind, row in jg[1].iterrows():
+			
+			fullcode = tri[int(row['TRCODE'])]
+			
+			day =  int(row['TUDIARYDAY']) - 1;
+			day =  (0 if int(row['TUDIARYDAY']) in weekdayset else 1);
+			dayset.add(day);
+			daycount[day] += 1;
+			
+			stop = np.floor(row['TUCUMDUR24']/5.0);
+			start = stop - np.floor(row['TUACTDUR24']/5.0)
+			startind = int(day * 288 + start)
+			stopind = int(day * 288 + stop)
+		
+			data[fullcode,startind:stopind] += 1;
+		
+			sum[startind:stopind] += 1;
+
+		#print("normalizing...")
+		for i in range(len(data)):
+			data[i] /= sum;
+		
+		print("plotting label "+ str(jg[0]) + ", c:"+str(labelscount[jg[0]]))
+		# x = np.arange(1.0,(8.0+(4.0/24.0)),(8.-1.0)/(288*7+48))
+		x = np.arange(0.0,7.0,1.0/288.0)
+		
+		additive = np.zeros(288*7)
+		
+		ax = plt.subplot(111)
+		box = ax.get_position()
+		ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+		ax.set_ylim([0.0,1.0])
+
+		datasum = np.cumsum(data, axis=0);
+		ax.fill_between(x,0,datasum[0],facecolor=colors[0],linewidth=0.1)
+		
+		for i in range(len(colors)-1):
+			ax.fill_between(x,datasum[i],datasum[i+1],facecolor=colors[i+1],linewidth=0.1)
+
+		plt.legend(legart,leglabels,loc='center left', bbox_to_anchor=(1, 0.5))
+		plt.title( "Label " +str(jg[0])+", count: "+str(labelscount[jg[0]]) )
+		F = plt.gcf();
+		F.set_size_inches(12,8)
+		F.set_dpi(300);
+		F.savefig(prefix +"label-" +str(jg[0]) + ".png");
+		plt.clf();
+
+
 
 
 def treeRefit(vector, labels):
@@ -108,12 +218,13 @@ def labelReduce(labels):
     newlabels = [labelmap[b] for b in labels];
     return newlabels,len(labelset);
 
-datapath = "/uufs/chpc.utah.edu/common/home/u0403692/prog/prism/data/timeuse/"
+datapath = "/uufs/chpc.utah.edu/common/home/u0403692/prog/prism/data/"
+tusepath = datapath + "timeuse/"
 
 print("reading...")
-acttable = pd.read_csv(datapath + "atusact_2015/atusact_2015.dat")
-infotable = pd.read_csv(datapath + "atusresp_2015/atusresp_2015.dat")
-rosttable = pd.read_csv(datapath + "atusrost_2015/atusrost_2015.dat")
+acttable = pd.read_csv(tusepath + "atusact_2015/atusact_2015.dat")
+infotable = pd.read_csv(tusepath + "atusresp_2015/atusresp_2015.dat")
+rosttable = pd.read_csv(tusepath + "atusrost_2015/atusrost_2015.dat")
 
 print("fixing roster...");
 
@@ -179,7 +290,6 @@ counts = np.zeros(actcount, dtype=np.uint32);
 
 randlabel = 3;
 
-
 for ind,i in enumerate(cases):
 	t = list(set(i[1]['mapped']))
 	vectors[ind][t] += 1;
@@ -244,8 +354,8 @@ compPlot(coords.T,dblabels,imgpath+"/dbscan");
 print("Random forest re-fitting...")
 refitclf,newlabels = treeRefit(supervector, dblabels);
 
-newlabelscount = Counter(newlabels);
-print("New label count: ",newlabelscount);
+labelscount = Counter(newlabels);
+print("New label count: ",labelscount);
 casecol['newlabels'] = newlabels;
 
 
@@ -253,169 +363,18 @@ casecol['newlabels'] = newlabels;
 compPlot(coords.T, newlabels, imgpath+"/dbscan-new");
 # casecol['newlabels'] = labels;
 # newlabels = labels
-# newlabelscount = Counter(newlabels);
+# labelscount = Counter(newlabels);
 	
 newjoin = pd.merge(jointable,casecol,on='TUCASEID')
-
 
 casecol.to_csv(imgpath+"/labels.csv")
 
 print("secondary processing...")
 
-
 clf = sklearn.tree.DecisionTreeClassifier(criterion='entropy',min_samples_split=2,min_samples_leaf=5,max_depth=8)
 clf = clf.fit(supervector,newlabels);
 writeTree(imgpath + "/dtreefinal.png",clf,[str(b) for b in mapping]+infocolumns)
 
-
-start = 0.0
-stop = 1.0
-number_of_lines=19
-cm_subsection = np.linspace(start, stop, number_of_lines)
-
-
-maincolors = [ cm.prism(x) for x in cm_subsection ]
-
-colors = []
-
-for i in mapping:
-	tier1 = i // 10000;
-	tier2 = (i // 100) - (tier1 * 100)
-	#print(tier2);
-	if (tier1 == 50): tier1 = 19;
-	if (tier2 == 99): tier2 = 19; 
-	tier1 = tier1 - 1;
-	tier2 = tier2 - 1
-	
-	scol = list(maincolors[tier1])
-# 	scol[2] = scol[2] + (tier2 * 0.1)
-# 	if(scol[2] > 1.0): scol[2] = 1.0;
-# 	scol[3] = scol[3] + (tier2 * 0.1)
-# 	if(scol[3] > 1.0): scol[3] = 1.0;
-	
-	
-	colors += [tuple(scol)];
-	 
-#print(colors[0:10]);
-
-
-legart = []
-leglabels=["Personal Care","HH activities","HH Member Care","NonHH care","work","Edu", "Consumerism", "Pro Services", "HH services", "Gov services", "Eating/Drinking","SocialRelaxLeisure","Sports/Excercise","Religious","Volunteering", "Telephone","Null","Traveling", "Other"]
-for i in range(number_of_lines):
-	legart += [mpatch.Rectangle((0,0),1,1,fc=maincolors[i])]
-
-newjoingroup = newjoin.groupby('newlabels')
-for njg in newjoingroup:
-	
-	if(newlabelscount[njg[0]] < 25):
-		continue;
-	
-	daycount = np.zeros(7)
-	data = np.zeros([actcount,288*7])
-	sum = np.zeros(288*7)
-	dayset = {1}
-	
-	#print(njg[1])
-	subsetnjg = njg[1][njg[1]['TUACTIVITY_N'] == 1]
-	mdem = subsetnjg.groupby(["TESEX","TEAGE"]).TESEX.count()[1]
-	fdem = subsetnjg.groupby(["TESEX","TEAGE"]).TESEX.count()[2]
-	#print(mdem,fdem);
-	if(type(mdem) == pd.core.series.Series): mdem.reindex(np.arange(0,101)).fillna(int(0)).plot(color='b')
-	else: print("bad mdem:",mdem,type(mdem))
-	if(type(fdem) == pd.core.series.Series): fdem.reindex(np.arange(0,101)).fillna(int(0)).plot(color='r')
-	else: print("bad fdem:",fdem,type(fdem))
-
-	plt.title( "Label " +str(njg[0])+", count: "+str(newlabelscount[njg[0]]) )
-	F = plt.gcf();
-	F.set_size_inches(4,3)
-	F.set_dpi(300);
-	F.savefig(imgpath + "/demo-" +str(njg[0]) + ".png");
-	plt.clf();
-	 
-	
-	for ind, row in njg[1].iterrows():
-		
-		fullcode = tri[int(row['TRCODE'])]
-		
-# 		codetier1 = int(row['TUTIER1CODE']) - 1
-# 		if(codetier1 > (18 - 1)):
-# 	# 	if(codetier1 !=(17)):
-# 			continue;
-	# 	
-	# 	codetier2 = int(row['TUTIER2CODE']) - 1;
-	# 	if(codetier2 == 98): codetier2 = 17 - 1;
-	# 	if(codetier2 > (18 - 1)):
-	# 		continue;
-	# 	 
-		day =  int(row['TUDIARYDAY']) - 1;
-		day =  (0 if int(row['TUDIARYDAY']) in weekdayset else 1);
-		dayset.add(day);
-		daycount[day] += 1;
-		#t = time.strptime(row['TUSTARTTIM'],'%H:%M:%S')
-		#start = np.floor((t.tm_hour*60+t.tm_min)/5)
-		#t = time.strptime(row['TUSTOPTIME'],'%H:%M:%S')
-		#stop = np.floor((t.tm_hour*60+t.tm_min)/5)
-		
-		
-		stop = np.floor(row['TUCUMDUR24']/5.0);
-		start = stop - np.floor(row['TUACTDUR24']/5.0)
-	# 	if(start == stop): #implicitly, we don't expect someone to do the same thing for 24 hours straight unless they are messed up
-	# 		stop = start + 1;
-		startind = int(day * 288 + start)
-		stopind = int(day * 288 + stop)
-		
-	# 	if(stop < start):
-	# 		stopind += 288;
-	# 		if(stopind > (day*288+288+48)):
-	# 			stopind = day*288+288+48;
-	
-	
-		data[fullcode,startind:stopind] += 1;
-	# 	data[codetier2,startind:stopind] += 1;
-	
-		sum[startind:stopind] += 1;
-		#else:
-			
-	# 		data[codetier1,startind:(day*288+288)] += 1;
-	# 		data[codetier1,(day*288):stopind] += 1;
-			
-	#print("normalizing...")
-	for i in range(len(data)):
-		data[i] /= sum;
-	
-	
-	#print("set of day vals: ",dayset);
-	
-	print("plotting label "+ str(njg[0]) + ", c:"+str(newlabelscount[njg[0]]))
-	# x = np.arange(1.0,(8.0+(4.0/24.0)),(8.-1.0)/(288*7+48))
-	x = np.arange(0.0,7.0,1.0/288.0)
-	
-	additive = np.zeros(288*7)
-	
-	ax = plt.subplot(111)
-	box = ax.get_position()
-	ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-	ax.set_ylim([0.0,1.0])
-	
-
-	datasum = np.cumsum(data, axis=0);
-	ax.fill_between(x,0,datasum[0],facecolor=colors[0],linewidth=0.1)
-	
-	for i in range(len(colors)-1):
-		ax.fill_between(x,datasum[i],datasum[i+1],facecolor=colors[i+1],linewidth=0.1)
-		#
-	# 	additive += data[i];
-	# 	plt.plot(x,datasum[i],linewidth=2.0,color=colors[i]);
-	# 	plt.plot(x,data[i],color=c,linewidth=4.0);
-	
-	
-	plt.legend(legart,leglabels,loc='center left', bbox_to_anchor=(1, 0.5))
-	plt.title( "Label " +str(njg[0])+", count: "+str(newlabelscount[njg[0]]) )
-	#plt.show();
-	F = plt.gcf();
-	F.set_size_inches(12,8)
-	F.set_dpi(300);
-	F.savefig(imgpath + "/label-" +str(njg[0]) + ".png");
-	plt.clf();
+def demoActPlot(newjoin, "newlabels", imgpath +"/")
 
 

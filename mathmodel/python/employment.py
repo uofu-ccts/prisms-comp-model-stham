303785ccfac67315bd52923129b9d3df04285658
u0403692 <a.m.lund@utah.edu>
2017-03-15 17:19:39 -0600

    implemented and tested employment probabability components in employment

--- mathmodel/python/employment.py
+++ mathmodel/python/employment.py
@@ -5,14 +5,15 @@ import collections;
 
 datapath = "/uufs/chpc.utah.edu/common/home/u0403692/prog/prism/data/"
 
+print("loading...")
 # #load indvs
-# con = sqlite3.connect(datapath + "indvs.sq3");
-# indvs = pd.read_sql("select * from indvs", con);
-# con.close();
+con = sqlite3.connect(datapath + "indvs2.sq3");
+indvs = pd.read_sql("select * from indvs", con);
+con.close();
 # #load locations
-# con = sqlite3.connect(datapath + "blockaddr.sq3");
-# blockaddr = pd.read_sql("select * from blockaddr", con);
-# con.close();
+con = sqlite3.connect(datapath + "blockaddr2.sq3");
+blockaddr = pd.read_sql("select * from blockaddr", con);
+con.close();
 #load LEHD tables
 odmat = pd.read_csv(datapath + "lehd/ut_od_main_JT00_2014.csv")
 #rac = pd.read_csv(datapath + "lehd/ut_rac_S000_JT00_2014.csv")
@@ -28,20 +29,24 @@ agetab = pd.read_csv(datapath + "employ/ACS_15_5YR_B23001_with_ann.csv",skiprows
 # print(rac)
 # print(wac)
 
+indvs = indvs.iloc[0:1000];
+
+
+print("building brackets...")
+
 #hours/weeks scheduling
 agebrackets = [16,20,22,25,30,35,45,55,60,62,65,70,75,101];
 
 ageindices = np.array([3,10,17,24,31,38,45,52,59,66,73,79,85])
 agediff = list(np.diff(ageindices) - 2);
 agediff += [agediff[len(agediff)-1]]
-print(agediff)
-ageshift = [0,92-3]
+#print(agediff)
+gendershift = [0,92-3]
 
 probbracket = []
-for i in ageshift:
+for i in gendershift:
 	temp = [];
 	for j,k in zip(ageindices,agediff):
-		print(j,k)
 		total = agetab['HD01_VD'+str(i+j).zfill(2)][0]
 		nonemploy = agetab['HD01_VD'+str(i+j+k).zfill(2)][0]
 		nonlabor = agetab['HD01_VD'+str(i+j+k+1).zfill(2)][0]
@@ -50,16 +55,56 @@ for i in ageshift:
 	probbracket += [temp]
 
 
-print(agebrackets);
-print(probbracket);
 
-hourbrackets = [0,1,15,35];
-weekbrackets = [0,1,14,27,40,48,50];
+#it's not terribly reasonable to have more than 70 hours
+#really the distribution is more complicated than simple breaks
+#I will probably need special distributions
+hourbrackets = [(1,15),(15,35),(35,60)];
+weekbrackets = [(50,52),(48,50),(40,48),(27,40),(14,27),(1,14)];
+hourweekbrackets = []
+for i in hourbrackets:
+	for j in weekbrackets:
+		hourweekbrackets += [(i,j)]
+
+def hourbracketbuild(table):
+	totalindices = [4,29];
+	hourindices = [19,12,5];
+	weekindices = range(1,7);
+	gendershift = [0,30-5];
+	bracket = []
+	for i,j in zip(gendershift,totalindices):
+		temp = [];
+		total = table['HD01_VD'+str(j).zfill(2)][0]
+		for k in hourindices:
+			for m in weekindices:
+				count = table['HD01_VD'+str(i+k+m).zfill(2)][0]
+				#print(count, str(i+k+m).zfill(2));
+				temp += [count / total];
+		bracket += [temp]
+	return bracket;
+
+youngbracket = hourbracketbuild(youngtab)
+oldbracket = hourbracketbuild(oldtab);
+
+
+#in the future I could interpolate with a lower bound cutoff 
+#instead of using the bracket system, but this is inconsequential at the moment
+def calcprob(x,agebrackets,prob):
+	
+	#calc employ prob
+	if(x.age < 16 and x.age >= 14): return 0.01;
+	if(x.age < 14): return 0.0;
+
+	for i in range(len(agebrackets) - 1):
+		if(x.age < agebrackets[i+1] and x.age >= agebrackets[i]):
+			p = prob[int(x.gender)][i];
+			p = 1.0 if p > np.random.random() else p;
+			return p;
+	return 0.0;
 
 
 
-exit()
-
+print("building outframe...")
 
 out = pd.DataFrame();
 
@@ -74,8 +119,12 @@ out['empcode'] = -1;
 out['hours']=0 #determines daily schedule
 out['weeks']=0 #determines weekly work prob
 out['shift']=0 #determines normal shift, or alternative shift
-out['probemploy'] = out.apply(calcprob,args=(ageprobtables), axis=1);
+out['probemploy'] = out.apply(calcprob, args=(agebrackets,probbracket), axis=1);
 
+
+
+print("assigning blocks...")
+print(out);
 
 gout = out.groupby(['block'])
 
@@ -85,7 +134,9 @@ c = 0;
 for g,df in gout:
 	#print(g)
 	#print(df)
-	df = df.sort(['age'])
+	df = df.sort_values('probemploy',axis=0,ascending=False)
+	
+	dfcount = np.sum(df['probemploy'] > 0.0);
 	#print(df)
 	if(g in odout.indices):
 		
@@ -102,17 +153,16 @@ for g,df in gout:
 		
 		np.random.shuffle(wlist);
 		
-		maxnum = len(df) if len(odg) > len(df) else len(odg)
+		#maxnum = len(df) if len(odg) > len(df) else len(odg)
+		maxnum = dfcount if len(odg) > dfcount else len(odg);
+		print('maxnum: ',maxnum);
 		out.loc[df.index[:maxnum],['empblock']] = wlist[:maxnum];
 		#print(out.iloc[df.index])
 
 # 	c+=1;
 # 	if(c == 3): break;
 
-
-
-
-
+print("assigning employment address...")
 	
 gout = out.groupby(['empblock'])
 bgroup = blockaddr.groupby(['block'])
@@ -148,10 +198,15 @@ for g,df in gout:
 # 	if(c == 3): break;
 
 
+print("writing...");
+
 out = out.drop('block',axis=1);
 out = out.drop('age',axis=1);
+out = out.drop('gender',axis=1);
+print(out);
 
-con = sqlite3.connect("/uufs/chpc.utah.edu/common/home/u0403692/prog/prism/data/employ.sq3");
+exit()
+con = sqlite3.connect("/uufs/chpc.utah.edu/common/home/u0403692/prog/prism/data/employ-"+time.strftime("%Y-%m-%d_%H-%M-%S")+".sq3");
 out.to_sql('employ',con);
 con.close();
 

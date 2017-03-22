d08ae8aa4eaced2590332cec0f26d8d9bf10839a
u0403692 <a.m.lund@utah.edu>
2017-03-22 09:55:29 -0600

    final version of actprofile for data generation in this iter

--- mathmodel/python/actprofile.py
+++ mathmodel/python/actprofile.py
@@ -94,22 +94,22 @@ def demoActPlot(frame,labelcolumn, prefix,savemats=False):
 		sum = np.zeros(288*2)
 		dayset = {1}
 		
-		#print(jg[1])
-		subsetjg = jg[1][jg[1]['TUACTIVITY_N'] == 1]
-		mdem = subsetjg.groupby(["TESEX","TEAGE"]).TESEX.count()[1]
-		fdem = subsetjg.groupby(["TESEX","TEAGE"]).TESEX.count()[2]
-		#print(mdem,fdem);
-		if(type(mdem) == pd.core.series.Series): mdem.reindex(np.arange(0,101)).fillna(int(0)).plot(color='b')
-		else: print("bad mdem:",mdem,type(mdem))
-		if(type(fdem) == pd.core.series.Series): fdem.reindex(np.arange(0,101)).fillna(int(0)).plot(color='r')
-		else: print("bad fdem:",fdem,type(fdem))
+		# #print(jg[1])
+		# subsetjg = jg[1][jg[1]['TUACTIVITY_N'] == 1]
+		# mdem = subsetjg.groupby(["TESEX","TEAGE"]).TESEX.count()[1]
+		# fdem = subsetjg.groupby(["TESEX","TEAGE"]).TESEX.count()[2]
+		# #print(mdem,fdem);
+		# if(type(mdem) == pd.core.series.Series): mdem.reindex(np.arange(0,101)).fillna(int(0)).plot(color='b')
+		# else: print("bad mdem:",mdem,type(mdem))
+		# if(type(fdem) == pd.core.series.Series): fdem.reindex(np.arange(0,101)).fillna(int(0)).plot(color='r')
+		# else: print("bad fdem:",fdem,type(fdem))
 
-		plt.title( "Label " +str(jg[0])+", count: "+str(labelscount[jg[0]]) )
-		F = plt.gcf();
-		F.set_size_inches(4,3)
-		F.set_dpi(300);
-		F.savefig(prefix + "demo-" +str(jg[0]) + ".png");
-		plt.clf();
+		# plt.title( "Label " +str(jg[0])+", count: "+str(labelscount[jg[0]]) )
+		# F = plt.gcf();
+		# F.set_size_inches(4,3)
+		# F.set_dpi(300);
+		# F.savefig(prefix + "demo-" +str(jg[0]) + ".png");
+		# plt.clf();
 		
 		
 		for ind, row in jg[1].iterrows():
@@ -134,9 +134,10 @@ def demoActPlot(frame,labelcolumn, prefix,savemats=False):
 		for i in range(len(data)):
 			data[i] /= sum;
 
-		ds = h5out.create_dataset('/label-'+str(jg[0]),data=data,fillvalue=0.,compression='gzip',compression_opts=9);
-		ds.attrs['label']=jg[0];
-		ds.attrs['prefix']=prefix;
+		if(savemats):
+			ds = h5out.create_dataset('/label-'+str(jg[0]),data=data,fillvalue=0.,compression='gzip',compression_opts=9);
+			ds.attrs['label']=jg[0];
+			ds.attrs['prefix']=prefix;
 		
 
 
@@ -144,9 +145,9 @@ def demoActPlot(frame,labelcolumn, prefix,savemats=False):
 		
 		print("plotting label "+ str(jg[0]) + ", c:"+str(labelscount[jg[0]]))
 		# x = np.arange(1.0,(8.0+(4.0/24.0)),(8.-1.0)/(288*7+48))
-		x = np.arange(0.0,7.0,1.0/288.0)
+		x = np.arange(0.0,2.0,1.0/288.0)
 		
-		additive = np.zeros(288*7)
+		additive = np.zeros(288*2)
 		
 		ax = plt.subplot(111)
 		box = ax.get_position()
@@ -167,7 +168,7 @@ def demoActPlot(frame,labelcolumn, prefix,savemats=False):
 		F.savefig(prefix +"label-" +str(jg[0]) + ".png");
 		plt.clf();
 
-	h5out.close();
+	if(savemats): h5out.close();
 
 
 
@@ -377,7 +378,7 @@ def determineLabels(frame, columns, name, path, cutoff = 50, eps=0.3, samples=10
 	labelframe[name+'labelreduce'] = reducedlabels;
 
 	if(savefinalclf):
-		joblib.dump(relabelclf, path+"/clfsave-"+name+".pkl");
+		joblib.dump(relabelclf, path+"/clfsave-"+name+".pkl", compress=('gzip',9));
 
 	featimport = featureSort(relabelclf, subframe.columns);
 
@@ -454,7 +455,7 @@ goodcols = ['TEAGE', 'TEHRUSL1', 'TELFS', 'TESCHENR', 'TESCHFT', 'TESCHLVL', 'TE
 fullcols = goodcols + ['TUDIARYDAY','10101', '10102', '10199', '10201', '10299', '10301', '10399', '10401', '20101', '20102', '20103', '20104', '20199', '20201', '20202', '20203', '20299', '20301', '20302', '20303', '20399', '20401', '20402', '20499', '20501', '20502', '20601', '20602', '20699', '20701', '20799', '20801', '20899', '20901', '20902', '20903', '20904', '20905', '20999', '29999', '30101', '30102', '30103', '30104', '30105', '30106', '30108', '30109', '30110', '30111', '30112', '30199', '30201', '30202', '30203', '30204', '30299', '30301', '30302', '30303', '30399', '30401', '30402', '30403', '30404', '30405', '30499', '30501', '30502', '30503', '30504', '30599', '40101', '40102', '40103', '40104', '40105', '40106', '40108', '40109', '40110', '40111', '40112', '40199', '40201', '40202', '40301', '40302', '40303', '40399', '40401', '40402', '40403', '40404', '40405', '40499', '40501', '40502', '40503', '40504', '40505', '40506', '40507', '40508', '40599', '49999', '50101', '50102', '50103', '50104', '50199', '50201', '50202', '50203', '50299', '50301', '50302', '50303', '50304', '50399', '50401', '50403', '50404', '50499', '59999', '60101', '60102', '60103', '60104', '60199', '60201', '60202', '60203', '60204', '60299', '60301', '60302', '60399', '60401', '60402', '60499', '69999', '70101', '70102', '70103', '70104', '70105', '70201', '80101', '80102', '80201', '80202', '80203', '80301', '80401', '80402', '80403', '80499', '80501', '80502', '80601', '80701', '80702', '89999', '90101', '90102', '90103', '90104', '90199', '90201', '90202', '90299', '90301', '90302', '90399', '90401', '90501', '90502', '90599', '99999', '100101', '100102', '100103', '100199', '100201', '100299', '100304', '100305', '100401', '110101', '110201', '120101', '120201', '120202', '120299', '120301', '120302', '120303', '120304', '120305', '120306', '120307', '120308', '120309', '120310', '120311', '120312', '120313', '120399', '120401', '120402', '120403', '120404', '120405', '120499', '120501', '120502', '120503', '120504', '130101', '130102', '130103', '130104', '130105', '130106', '130107', '130108', '130109', '130110', '130112', '130113', '130114', '130115', '130116', '130117', '130118', '130119', '130120', '130122', '130124', '130125', '130126', '130127', '130128', '130129', '130130', '130131', '130132', '130133', '130134', '130135', '130136', '130199', '130202', '130203', '130207', '130210', '130212', '130213', '130216', '130218', '130219', '130220', '130222', '130223', '130224', '130225', '130226', '130227', '130229', '130232', '130299', '130301', '130302', '139999', '140101', '140102', '140103', '140105', '149999', '150101', '150102', '150103', '150104', '150105', '150106', '150199', '150201', '150202', '150203', '150204', '150299', '150301', '150302', '150399', '150401', '150402', '150501', '150601', '150602', '150699', '150701', '150801', '159999', '160101', '160102', '160103', '160104', '160105', '160106', '160107', '160108', '160199', '160201', '180101', '180201', '180202', '180203', '180204', '180205', '180206', '180207', '180208', '180209', '180299', '180301', '180302', '180303', '180304', '180305', '180401', '180402', '180403', '180404', '180405', '180499', '180501', '180502', '180503', '180504', '180599', '180601', '180602', '180603', '180604', '180699', '180701', '180702', '180703', '180704', '180801', '180802', '180803', '180804', '180805', '180806', '180807', '180899', '180901', '180902', '180903', '180904', '180905', '180999', '181001', '181002', '181101', '181201', '181202', '181203', '181204', '181205', '181299', '181301', '181302', '181399', '181401', '181501', '181599', '181601', '181801', '189999', '500101', '500103', '500105', '500106', '500107']
 
 
-casetype,firstimport = determineLabels(superframe, goodcols, "casetype", imgpath, cutoff = 65, eps=0.3,samples=10,perplex=5,savefinalclf=True);
+casetype,firstimport = determineLabels(superframe, goodcols, "casetype", imgpath, cutoff = 35, eps=0.3,samples=10,perplex=30,savefinalclf=True);
 
 daytype,secondimport = determineLabels(superframe, fullcols, "daytype", imgpath, cutoff = 35,eps=0.2,samples=20,perplex=30);
 
@@ -469,14 +470,14 @@ daytype,secondimport = determineLabels(superframe, fullcols, "daytype", imgpath,
 #superframe['daytype']=secondcol['secondlabelreduce'];
 
 casecol = pd.merge(casetype,daytype,on='TUCASEID')
-casecol = pd.merge(casecol, superframe[['TUDIARYDAY','TUCASEID']],on='TUCASEID')
+outcol = pd.merge(casecol, superframe[['TUDIARYDAY','TUCASEID']],on='TUCASEID')
 
 print(firstimport)
 print(secondimport)
 #print(featureSort(finalclf,subframe.columns));
 #print("Percent same labels ", np.sum(casecol['finallabels']==casecol['secondlabelreduce'])/casecount*100)
 
-casecol.to_csv(imgpath+"/labels.csv")
+outcol.to_csv(imgpath+"/labels.csv")
 
 
 print("secondary processing...")

2c3dc11f55d425ed389f718dfefe411a6ec73e00
u0403692 <a.m.lund@utah.edu>
2017-03-25 14:13:05 -0600

    final labeltab with output to mats

--- mathmodel/python/labeltab.py
+++ mathmodel/python/labeltab.py
@@ -1,11 +1,11 @@
 import pandas as pd;
 import numpy as np;
-import matplotlib.pyplot as plt;
+import h5py;
+#import matplotlib.pyplot as plt;
 
 def arrayify(table):
-	g = labels.groupby(["casetypelabelreduce","daytypelabelreduce"])
+	g = table.groupby(["casetypelabelreduce","daytypelabelreduce"])
 	c = g.casetypelabelreduce.count()
-	print(c)
 	xy,z = zip(*c.iteritems())
 	x,y = zip(*xy)
 	z_arr = np.zeros((max(x)+1,max(y)+1))
@@ -13,11 +13,12 @@ def arrayify(table):
 	y = np.array(y)
 	z = np.array(z)
 	z_arr[x,y]=z
-	
+	for i in range(len(z_arr)):
+		z_arr[i] = z_arr[i] / np.sum(z_arr[i])
 	return z_arr;
 
 
-datapath = "/uufs/chpc.utah.edu/common/home/u0403692/prog/prism/data/final-label-classifier"
+datapath = "/uufs/chpc.utah.edu/common/home/u0403692/prog/prism/data/final-label-classifier/"
 
 labels = pd.read_csv(datapath+"labels.csv")
 
@@ -27,12 +28,20 @@ weekday = labels[labels['TUDIARYDAY'].apply(lambda x: x in (2,3,4,5,6))]
 weekendarr = arrayify(weekend);
 weekdayarr = arrayify(weekday);
 
-ax1 = plt.subplot(1,2,1)
-im1 = ax1.matshow(weekdayarr,cmap='viridis_r')
-plt.colorbar(im1);
-ax2 = plt.subplot(1,2,2)
-im2 = ax2.matshow(weekendarr,cmap='viridis_r')
-plt.colorbar(im2)
-plt.show()
+out = h5py.File(datapath + "labeltab.h5");
+ds = out.create_dataset("/weekend",data=weekendarr,fillvalue=0.,compression='gzip',compression_opts = 9);
+ds = out.create_dataset("/weekday",data=weekdayarr,fillvalue=0.,compression='gzip',compression_opts = 9);
+out.close();
+
+# ax1 = plt.subplot(1,3,1)
+# im1 = ax1.matshow(weekdayarr,cmap='Greys')
+# plt.colorbar(im1);
+# ax2 = plt.subplot(1,3,2)
+# im2 = ax2.matshow(weekendarr,cmap='Greys')
+# plt.colorbar(im2)
+# ax3 = plt.subplot(1,3,3)
+# im3 = ax3.matshow(weekdayarr-weekendarr,cmap='seismic')
+# plt.colorbar(im3)
+# plt.show()
 
 

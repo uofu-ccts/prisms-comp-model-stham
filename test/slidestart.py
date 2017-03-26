5435b38b0529a9665c77bf15426a0184946b8265
u0403692 <a.m.lund@utah.edu>
2017-03-26 15:13:55 -0600

    final version of slidestart

--- test/slidestart.py
+++ test/slidestart.py
@@ -3,7 +3,11 @@ import time;
 import matplotlib.pyplot as plt;
 import matplotlib.cm as cm
 import matplotlib.patches as mpatch;
+#import scipy.stats.norm as norm;
+from scipy.stats import norm;
 import numpy as np;
+
+import h5py;
 
 
 def slide(x,width):
@@ -32,36 +36,86 @@ def slide2(x,width,count):
 
 
 #table to be sorted, width of window, interval size
-def slidewindow(table, bandwidth, interval_size):
+def slidewindow(table, maxinstances, interval_size,actmapping,locmapping):
 
 	intervals = 1440 / interval_size;
-	actcount = max(list(table['actind'])) + 1
-	totalcount = len(set(table['TUCASEID']));
+	actcount = len(actmapping);
+
+	casemap = np.sort(list(set(table['TUCASEID'])))
+	casecount = len(casemap)
+
+	cri = { tr:i for i,tr in enumerate(casemap) }
+	itc = { i:tr for i,tr in enumerate(casemap) }
+
+	#calc basecounts
+
+	basecount = np.zeros((actcount,casecount))
+	for i,df in table.iterrows():
+		basecount[df.actind,cri[df.TUCASEID]] += 1
+
 	
-	#calc starttimes
-	startcount = np.zeros((actcount, intervals));
-	verticalsum = np.zeros(intervals);
+	
+	avginstances = np.zeros((actcount,2));
+	for i in range(len(basecount)):
+		if(np.sum(basecount[i]) < 1): continue;
+		p = norm.fit(basecount[i]);
+		# a,b = p;
+		avginstances[i] =  p
+		# x = np.linspace(-5.,5.,1000);
+		# plt.plot(x,norm.pdf(x,a,b));
+	
+	# print(avginstances);
+	# plt.show();
+	
+	#calc starttimes cohorts
+	startcount = np.zeros((actcount,maxinstances, intervals));
+	# instancecount = np.zeros((actcount,maxinstances));
+
+	table.sort_values(['actind','TUCASEID','TUACTIVITY_N'],inplace=True)
+
+	table['instance'] = table.groupby(['actind','TUCASEID']).cumcount();
+
+	#print(table[['actind','TUCASEID','TUACTIVITY_N','instance']].iloc[0:100])
 
 	for i,df in table.iterrows():
+		if(df.instance >= maxinstances): continue;
 		index = np.floor(df.start / interval_size)
-		startcount[df.actind,index] += 1;
-		verticalsum[index] += 1;
+		startcount[df.actind,df.instance,index] += 1;
+		# instancecount[df.actind,df.instance] += 1;
+
 	
-	# for i in range(len(startcount)):
-	# 	startcount[i] /= verticalsum;
+	
+	for i in range(len(startcount)):
+		for j in range(len(startcount[i])):
+			startcount[i,j] = np.cumsum(startcount[i,j]);
+			# startcount[i,j] /= np.amax(startcount[i,j]);
+
+
+
+	#get the location information
+	loccount = len(locmapping);
+	locations = np.zeros((actcount,loccount));
+	for i,df in table.iterrows():
+		locations[df.actind,df.whereind] += 1;
+
+	for i in range(len(locations)):
+		locations[i] /= np.sum(locations[i]);
+		# plt.plot(locations[i])
+	
+	# plt.show();
 
 	#compute the slider
 
-	slidewidth = np.floor(bandwidth / interval_size);
+	#slidewidth = np.floor(bandwidth / interval_size);
 
 	#convert = np.convolve(verticalsum,np.ones(slidewidth,dtype=int),'same')
 
-	for i in range(len(startcount)):
+	#for i in range(len(startcount)):
 		#startcount[i] = np.convolve(startcount[i],np.ones(slidewidth,dtype=int),'same')
-		startcount[i] = slide2(startcount[i],0.95,totalcount);
+		#startcount[i] = slide2(startcount[i],0.95,casecount);
 		#startcount[i] /= totalcount;
 
-	return startcount;
+	return avginstances,startcount,locations;
 
 def plotslidewindowOLD(mat,mapping,title,path):
 	
@@ -147,7 +201,7 @@ def plotslidewindow(mat,mapping,title,path):
 		legart += [mpatch.Rectangle((0,0),1,1,fc=maincolors[i])]
 
 
-	intervals = len(mat[0]);
+	intervals = len(mat[0][0]);
 
 	x = np.linspace(0.0,24.0,intervals)
 	
@@ -165,7 +219,10 @@ def plotslidewindow(mat,mapping,title,path):
 	# for i in range(len(colors)-2):
 	# 	ax.fill_between(x,datasum[i],datasum[i+1],facecolor=colors[i+1],linewidth=0.1)
 	for i in range(len(mat)):
-		ax.plot(x,mat[i],color=colors[i])
+		for j in range(len(mat[i])):
+		# for j in range(1):	
+			ax.plot(x,mat[i,j],color=colors[i])
+		# if(i >): break;
 
 	plt.legend(legart,leglabels,loc='center left', bbox_to_anchor=(1, 0.5))
 	plt.title(title)
@@ -187,22 +244,39 @@ acttable = pd.read_csv(datapath + "timeuse/atusact_2015/atusact_2015.dat")
 labeltab = pd.read_csv(datapath + "final-label-classifier/labels.csv")
 
 acttable = pd.merge(acttable,labeltab,on="TUCASEID");
-acttable = acttable[['TUCASEID','TRCODE','daytypelabelreduce','TEWHERE','TUACTDUR24','TUCUMDUR24']]
+acttable = acttable[['TUCASEID','TRCODE','daytypelabelreduce','TEWHERE','TUACTDUR24','TUCUMDUR24','TUACTIVITY_N']]
 
-mapping = np.sort(list(set(acttable['TRCODE'])))
-actcount = len(mapping)
+actmapping = np.sort(list(set(acttable['TRCODE'])))
+#actcount = len(mapping)
 
-tri = { tr:i for i,tr in enumerate(mapping) }
-itr = { i:tr for i,tr in enumerate(mapping) }
+ati = { tr:i for i,tr in enumerate(actmapping) }
+ita = { i:tr for i,tr in enumerate(actmapping) }
+
+locmapping = np.sort(list(set(acttable['TEWHERE'])))
+wti = { tr:i for i,tr in enumerate(locmapping) }
+itw = { i:tr for i,tr in enumerate(locmapping) }
+# print([(i,k) for i,k in enumerate(locmapping)]);
 
 acttable['start'] = acttable['TUCUMDUR24']-acttable['TUACTDUR24']
-acttable['actind'] = acttable['TRCODE'].apply(lambda x: tri[x]);
+acttable['actind'] = acttable['TRCODE'].apply(lambda x: ati[x]);
+acttable['whereind'] = acttable['TEWHERE'].apply(lambda x: wti[x]);
 
 #print(acttable);
 
+outfile = h5py.File(datapath + "actdata.h5");
+
+outfile.create_dataset("/mapping",data=locmapping,fillvalue=0.,compression='gzip',compression_opts=9)
+
 
 for i,df in acttable.groupby(['daytypelabelreduce']):
+	print(i,end=' ');
+	avginstances,priormat,locations = slidewindow(df,5,5,actmapping,locmapping)
 
-	slidemat = slidewindow(df,180,5)
-	plotslidewindow(slidemat,mapping,"label "+str(i),"");
+	outfile.create_dataset("/label-"+str(i)+"/avginstances",data=avginstances,fillvalue=0.,compression='gzip',compression_opts=9)
+	outfile.create_dataset("/label-"+str(i)+"/priorities",data=priormat,fillvalue=0.,compression='gzip',compression_opts=9)
+	outfile.create_dataset("/label-"+str(i)+"/locations",data=locations,fillvalue=0.,compression='gzip',compression_opts=9)
 
+	
+	#plotslidewindow(slidemat,actmapping,"label "+str(i),"");
+
+outfile.close();

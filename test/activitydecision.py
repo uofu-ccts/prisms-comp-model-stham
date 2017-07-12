7f82d3c47d98273e44fe6a94c50929827a7726ba
u0403692 <a.m.lund@utah.edu>
2017-07-12 12:50:53 -0600

    working actnext and debugged actprop

--- test/activitydecision.py
+++ test/activitydecision.py
@@ -4,37 +4,91 @@ import matplotlib.pyplot as plt;
 import matplotlib.cm as cm
 import matplotlib.patches as mpatch;
 #import scipy.stats.norm as norm;
-from scipy.stats import halfnorm;
+from scipy.stats import halfnorm, norm;
 import numpy as np;
 import sys;
 
 def propkernel(start,end,edgeblock):
 	edgewidth = len(edgeblock);
 	out = np.zeros((1440,));
-	out[start:end] = 1.0;
-	out[max((start-edgewidth),0):start] = edge;
-	out[end:min(1440,(end+edgewidth))] = edge[::-1];
 
-def propkerngauss(sidewidth):
+
+	out[start:end] += 1.0;
+	
+	
+	sedge = max(start-edgewidth,0);
+	slen = min(start, edgewidth);
+	eedge = min(1440,(end+edgewidth))
+	elen = min((1440-end), edgewidth)
+
+	out[sedge:start] += edgeblock[:slen][::-1];
+	out[end:eedge] += edgeblock[:elen];
+
+	return out;
+
+def propkernhalfgauss(sidewidth):
 	invals = np.linspace(0.0,3.0,sidewidth);
-	out = halfnorm(invals);
+	out = halfnorm.pdf(invals);
+	# print(out/out.max());
 	return out/out.max();
 
-def actprop(acttable, actmapping, sidewidth=20):
+def actprop(acttable, actmapping, sidewidth=20, limit=0.25):
 
 	actcount = len(actmapping);
 
-	casemap = np.sort(list(set(table['TUCASEID'])))
+	casemap = np.sort(list(set(acttable['TUCASEID'])))
 	casecount = len(casemap)
 
 	out = np.zeros((actcount,1440));
 
-	edgeblock = propkerngauss(sidewidth);
+	edgeblock = propkernhalfgauss(sidewidth);
 
 	for i,df in acttable.iterrows():
 		out[df.actind] += propkernel(df.start,df.end,edgeblock);
+	
+	# for i in range(len(out)):
+	# 	out[i] = out[i] / (out[i].max()+0.0001);
+	# out = out/float(casecount);
+	limitcount = limit*casecount
+	out = np.clip(out,0.0,limitcount)/limitcount;
 
-	out = out/float(casecount);
+	return out;
+
+
+def nextkernel(start,length):
+	
+	klength = length*2.0
+
+	kern = norm.pdf(np.linspace(-3.0,3.0,klength));
+	out = np.zeros((1440,));
+
+	
+	
+	sedge = max(0,start-length);
+	eedge = min(1440,start+length);
+	skern = max(0, length-start);
+	ekern = min(klength, klength + 1440 - start)
+
+	# print(start, length ,sedge, eedge, skern, ekern);
+
+	out[sedge:eedge] = kern[skern:ekern]
+
+	return out;
+
+def actnext(acttable, actmapping, sidelen=0.5, limit=0.25):
+
+	actcount = len(actmapping);
+
+	casemap = np.sort(list(set(acttable['TUCASEID'])))
+	casecount = len(casemap)
+
+	out = np.zeros((actcount,1440));
+
+	for i,df in acttable.iterrows():
+		out[df.actind] += nextkernel(df.start,np.ceil(df.length*sidelen));
+
+	limitcount = limit*casecount
+	out = np.clip(out,0.0,limitcount)/limitcount;
 
 	return out;
 
@@ -57,6 +111,7 @@ actmapping = np.sort(list(set(acttable['TRCODE'])))
 #actcount = len(mapping)
 ati = { tr:i for i,tr in enumerate(actmapping) }
 ita = { i:tr for i,tr in enumerate(actmapping) }
+print([(i,k) for i,k in enumerate(actmapping)]);
 
 locmapping = np.sort(list(set(acttable['TEWHERE'])))
 wti = { tr:i for i,tr in enumerate(locmapping) }
@@ -66,17 +121,20 @@ itw = { i:tr for i,tr in enumerate(locmapping) }
 labellist = np.sort(list(set(acttable['daytypelabelreduce'])))
 
 acttable['start'] = acttable['TUCUMDUR24']-acttable['TUACTDUR24']
-acttable['end'] = acttable['TUACTDUR24']
+acttable['end'] = acttable['TUCUMDUR24']
+acttable['length'] = acttable['TUACTDUR24']
 acttable['actind'] = acttable['TRCODE'].apply(lambda x: ati[x]);
+
+
 
 for i,df in acttable.groupby(['daytypelabelreduce']):
 	c = 0;
 	if (c > 3): break;
-
-	prop = actprop(acttable,actmapping);
+	print("calc:",i)
+	prop = actnext(df,actmapping,sidelen=0.5,limit=0.15);
 
 	plt.matshow(prop);
 	plt.show();
-	plt.clf();
+	
 	
 	c += 1

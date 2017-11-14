0553f0ec31aee8163d18abcca37674b7aee0a462
u0403692 <a.m.lund@utah.edu>
2017-11-14 14:55:41 -0700

    cosmetic changes

--- mathmodel/python/labeltab.py
+++ mathmodel/python/labeltab.py
@@ -1,7 +1,7 @@
 import pandas as pd;
 import numpy as np;
 import h5py;
-#import matplotlib.pyplot as plt;
+# import matplotlib.pyplot as plt;
 
 def arrayify(table):
 	g = table.groupby(["casetypelabelreduce","daytypelabelreduce"])
@@ -33,15 +33,15 @@ ds = out.create_dataset("/weekend",data=weekendarr,fillvalue=0.,compression='gzi
 ds = out.create_dataset("/weekday",data=weekdayarr,fillvalue=0.,compression='gzip',compression_opts = 9);
 out.close();
 
-ax1 = plt.subplot(1,3,1)
-im1 = ax1.matshow(weekdayarr,cmap='Greys')
-plt.colorbar(im1);
-ax2 = plt.subplot(1,3,2)
-im2 = ax2.matshow(weekendarr,cmap='Greys')
-plt.colorbar(im2)
-ax3 = plt.subplot(1,3,3)
-im3 = ax3.matshow(weekdayarr-weekendarr,cmap='seismic')
-plt.colorbar(im3)
-plt.show()
+# ax1 = plt.subplot(1,3,1)
+# im1 = ax1.matshow(weekdayarr,cmap='Greys')
+# plt.colorbar(im1);
+# ax2 = plt.subplot(1,3,2)
+# im2 = ax2.matshow(weekendarr,cmap='Greys')
+# plt.colorbar(im2)
+# ax3 = plt.subplot(1,3,3)
+# im3 = ax3.matshow(weekdayarr-weekendarr,cmap='seismic')
+# plt.colorbar(im3)
+# plt.show()
 
 

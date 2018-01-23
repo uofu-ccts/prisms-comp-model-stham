843720cf699d99da9eaff059679bb5a474225d51
u0403692 <a.m.lund@utah.edu>
2018-01-23 15:58:45 -0700

    adding script for merging acttraj into single folder

---
+++ test/sqlitemerge.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NEWDB=${1/.sqlite3/}.merge.sqlite3
+echo $NEWDB
+
+sqlite3 $1 '.schema acttraj' | sqlite3 $NEWDB
+
+for f in "$@"
+do
+	echo $f
+	sqlite3 $NEWDB "attach '$f' as tmerge; begin; insert into acttraj select * from tmerge.acttraj; commit; detach tmerge;"
+done

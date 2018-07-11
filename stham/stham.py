40d863b887da6755022478b36b7ddf2ccb5e0fef
u0403692 <a.m.lund@utah.edu>
2018-07-11 14:01:14 -0600

    adding config and main

---
+++ stham/stham.py
@@ -0,0 +1,21 @@
+import configparser;
+import argparse;
+
+
+#parse arguments
+parser = argparse.ArgumentParser(description="Runs the STHAM agent based exposure model.")
+
+
+args = parser.parse_args()
+
+#create default ini
+
+
+#run ini
+
+
+#version?
+
+
+
+

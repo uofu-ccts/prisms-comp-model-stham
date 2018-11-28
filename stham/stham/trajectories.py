54bb34bf3a0cf6d66d3b5fa27c3945878bb8f74f
u0403692 <a.m.lund@utah.edu>
2018-11-28 16:14:11 -0700

    partial working version of trajectory generation

--- stham/stham/trajectories.py
+++ stham/stham/trajectories.py
@@ -2,36 +2,8 @@ import pandas as pd;
 import numpy as np;
 import rtree;
 import locations
-from enum import Enum
-
-
-class PTe(Enum):
-	ACTCOUNT = 0
-	LENCOUNT = 1
-	LENACTJOINTPROB = 2
-	ACTWINS = 3
-	LENWINS = 4
-	LHIST = 5
-	LBINS = 6
-	LHISTLEN = 7
-	ORDERPROB = 8
-	WHEREPROB = 9
-
-
-class AWe(Enum):
-	ACTCODE = 0
-	ACTPROB = 1
-	WMIN = 2
-	WMAX = 3
-	WAVG = 4
-	LENS = 5 #only in newtraj
-	PREC = 6 #only in newtraj
-	LMAX = 7 #only in newtraj
-	LOC = 8 #only in newtraj
-
-class LWe(Enum):
-	LMIN = 0
-	LMAX = 1
+import joblib
+from enums import PTe,AWe,LWe;
 
 
 
@@ -54,44 +26,55 @@ def precsort(precede):
 
 def buildtraj(agent, locs, ptab, tripmethod=None):
 	
-	
 	if(tripmethod == None):
 		tripmethod = None
 
-	wherecount = len(PTe.WHEREPROB[0])
+	wherecount = len(ptab[PTe.WHEREPROB][0])
 	lengths = np.zeros(ptab[PTe.ACTCOUNT])
 	lmaxv = np.zeros(ptab[PTe.ACTCOUNT])
 	where = np.zeros(ptab[PTe.ACTCOUNT])
 	for i in range(ptab[PTe.ACTCOUNT]):
 		lwinpick = np.random.choice(ptab[PTe.LENCOUNT],p=ptab[PTe.LENACTJOINTPROB][i])
-		lhistpick = np.random.choice(ptab[PTe.LHISTLEN],p=ptab[PTe.LHIST][lwinpick])
+		lhistpick = np.random.choice(len(ptab[PTe.LHIST][lwinpick]),p=ptab[PTe.LHIST][lwinpick])
 		low = ptab[PTe.LBINS][lwinpick][lhistpick]
 		high = ptab[PTe.LBINS][lwinpick][lhistpick + 1]
 		lengths[i] = np.random.rand() * (high-low) + low
-		lmaxv[i] = ptab[PTe.LENWINS][lwinpick][LWe.LMAX]
-		where[i] = np.random.choice(wherecount,p=ptab[Pte.WHEREPROB][i])
+		lmaxv[i] = ptab[PTe.LENWINS][lwinpick][LWe.LMAX.value]
+		where[i] = np.random.choice(wherecount,p=ptab[PTe.WHEREPROB][i])
 
 	precorder = precsort(ptab[PTe.ORDERPROB])
 	newtraj = np.copy(ptab[PTe.ACTWINS])
-	newtraj = np.append(newtraj,lengths,axis=0)
-	newtraj = np.append(newtraj,precorder,axis=0)
-	newtraj = np.append(newtraj,lmaxv,axis=0)
-	newtraj = np.append(newtraj,where,axis=0)
-	sorttraj = np.lexsort(newtraj[[AWe.WMAX,AWe.LENS,AWe.WMIN,AWe.WAVG,AWe.PREC]])
-	newtraj = newtraj.T[sorttraj].T
+	# print(newtraj[0])
+	newtraj = np.append(newtraj,lengths.reshape(ptab[PTe.ACTCOUNT],1),axis=1)
+
+	newtraj = np.append(newtraj,precorder.reshape(ptab[PTe.ACTCOUNT],1),axis=1)
+	newtraj = np.append(newtraj,lmaxv.reshape(ptab[PTe.ACTCOUNT],1),axis=1)
+	newtraj = np.append(newtraj,where.reshape(ptab[PTe.ACTCOUNT],1),axis=1)
+	sorttraj = np.lexsort(newtraj.T[[AWe.WMAX.value,AWe.LENS.value,AWe.WMIN.value,AWe.WAVG.value,AWe.PREC.value]])
+	# print(sorttraj)
+	print(newtraj)
+	newtraj = newtraj[sorttraj].T
+	print(newtraj)
 
 	#pickwins up to 3 times
 	actpicks = np.full(ptab[PTe.ACTCOUNT],False)
 	validwin = np.full(ptab[PTe.ACTCOUNT],True)
+	actprob = newtraj[AWe.ACTPROB.value]
 	for i in range(3):
-		actpicks = actpicks | ((np.random.rand(ptab[PTe.ACTCOUNT]) < newtraj[AWe.ACTPROB]) & validwin)
-		ends = np.cumsum(newtraj[AWe.LENS]*actpicks)
-		starts = (ends*actpicks) - newtraj[AWe.LENS]
-		validwin = True if (start <= newtraj[AWe.WMAX] and start >= newtraj[AWe.WMIN]) else False
-		if(newtraj[AWe.LMAX]*actpicks).sum() > 1440:
-			break;
+		print(actprob)
+		actpicks = actpicks | ((np.random.rand(ptab[PTe.ACTCOUNT]) < actprob ) & validwin)
+		ends = np.cumsum(newtraj[AWe.LENS.value]*actpicks)
+		starts = (ends*actpicks) - newtraj[AWe.LENS.value]
+		
+		validwin = (starts <= newtraj[AWe.WMAX.value]) & (starts >= newtraj[AWe.WMIN.value])
 
-	newtraj = newtraj.T[actpicks].T
+		if(np.sum(newtraj[AWe.LMAX.value]*actpicks)) > 1440:
+			break;
+		else:
+			#mass adjustment
+			actprob += 0.2
+	print(np.sum(newtraj[AWe.LMAX.value]*actpicks),actpicks)
+	newtraj = newtraj.T[actpicks]
 		
 
 	#setlocs
@@ -111,16 +94,10 @@ def buildtraj(agent, locs, ptab, tripmethod=None):
 
 def runit():
 
+	#FIXME: remove load or add test path
+	ptab = joblib.load("windowtest.gz")
 
-	# for i in range(10):
-		
-
-	# agent = 0;
-	# loc = 0;
-	# ptab = {ACTCOUNT:10,LENCOUNT:,LENACTJOINTPROB:,ACTWINS:,:LENWINS:,LHIST:,LBINS:,LHISTLEN:,ORDERPROB:}
-
-
-	pass;
+	print(buildtraj(0,0,ptab));
 
 
 if __name__=="__main__":
